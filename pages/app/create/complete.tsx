e7b64c1723ceff4a4ff022a3fde75a545479e01e
Dhairya Shah <dtshah24022006@gmail.com>
2023-05-18 09:35:07 +0530

    add loading to qr code

--- pages/app/create/complete.tsx
+++ pages/app/create/complete.tsx
@@ -78,12 +78,35 @@ export default function Complete() {
               width={180}
               height={180}
             /> */}
+            { value ? 
             <QRCode
               logoOnLoad={() => console.log("Loaded")}
               value={value ? value : "loading"}
               fgColor="#072AC8"
               qrStyle="dots"
-            />
+            />: <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="animate-spin"
+            width="80"
+            height="80"
+            viewBox="0 0 24 24"
+            strokeWidth="1"
+            stroke="currentColor"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M12 6l0 -3"></path>
+            <path d="M16.25 7.75l2.15 -2.15"></path>
+            <path d="M18 12l3 0"></path>
+            <path d="M16.25 16.25l2.15 2.15"></path>
+            <path d="M12 18l0 3"></path>
+            <path d="M7.75 16.25l-2.15 2.15"></path>
+            <path d="M6 12l-3 0"></path>
+            <path d="M7.75 7.75l-2.15 -2.15"></path>
+          </svg>
+}
             <h3 className="text-lg font-semibold sm:text-2xl">QR Code Generated</h3>
             <p className="text-center text-textSecondary">
               Now share this QR Code across your network
