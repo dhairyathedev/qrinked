a04a2962301603244cb1f38aa1d6be160c865aea
Dhairya Shah <dtshah24022006@gmail.com>
2023-05-22 14:49:25 +0530

    major: shift from .mjs to .js in next.config.mjs file

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+// const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-let assetPrefix = ""
-let basePath = "/"
+// let assetPrefix = ""
+// let basePath = "/"
 
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "")
+// if (isGithubActions) {
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "")
 
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
+//   assetPrefix = `/${repo}/`
+//   basePath = `/${repo}`
+// }
 
 const nextConfig = {
   reactStrictMode: false,
